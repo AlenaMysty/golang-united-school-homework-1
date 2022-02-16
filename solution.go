346e7c526c58a8af3b0a27a27587acab7b3c72c5
AlenaMysty <yackiv.a.v@gmail.com>
2022-02-16 11:16:05 +0200

    Delete main

--- solution.go
+++ solution.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/kyokomi/emoji"
 )
 
-func main() {
-	worldMessage := GetMessage()
-	fmt.Println(worldMessage)
-}
 func GetMessage() string {
 	return emoji.Sprint("Hello :world_map:!")
 }
